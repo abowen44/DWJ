824c560d145c2812071e61dfc2887c102db687d5
abowen44 <abowen44@ivytech.edu>
2026-09-02 00:01:30 -0400

    Add files via upload

---
+++ DWJ/Ch1/Hello2.java
@@ -0,0 +1,11 @@
+package Ch1;
+// Alexis Bowen
+// p. 17
+// Filename Hello2.java
+// Written on Aug 31, 2026
+public class Hello2 {
+   
+   public static void main(String[] args) {
+        System.out.println("Hello, world!");
+   }
+}
---
+++ DWJ/Ch2/ArithmeticDemo.java
@@ -0,0 +1,30 @@
+// Alexis Bowen
+// p.71 Aug 31, 2026
+
+{
+    public static void mian(String[] args)
+    {
+        int firstNumber;
+        int secondNumber;
+        int sum;
+        int difference;
+        int average;
+        Scanner input = new Scanner(System.in);
+        System.out.print(s: "Please enter an integer >> ");
+        firstNumber = input.nextInt();
+        System.out.print(s: "Please enter another integer >> ");
+        secondNumber = input.nextInt();
+        sum = firstNumber + secondNumber;
+        difference = firstNumber - SecondNumber;
+        average = sum / 2;
+        System.out.println(firstNumber + "+" + 
+            secondNumber + "is " + sum);
+        System.out.println(firstNumber + " - " +
+            secondNumber + " is " + difference);
+        System.out.print.ln()
+        )
+        )
+
+
+    }
+}
