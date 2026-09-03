373b79e019e4aabd1291dd144ebef720b0a2cecf
abowen44 <abowen44@ivytech.edu>
2026-09-02 22:34:43 -0400

    Add files via upload

---
+++ DWJ/Ch1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+// Alexis Bowen
+// p 35 Exercise 7 
+
+public class MovieQuoteInfo {
+    public static void main(String [] args) {
+        System.out.println("Famous quote: \"Whoever said orange is the new pink was seriously disturbed.\"");
+        System.out.println("Name of the movie: Legally Blonde");
+        System.out.println("Name of the character: Elle woods");
+        System.out.println("Year released: 2001");
+    }
+    
+}
